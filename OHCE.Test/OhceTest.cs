949e2f69d8343ed70950eea90b6d074168a62b8c
Enzo Sandre <enzo@enzosandre.fr>
2025-06-10 16:53:36 +0200

    [RED] Bien dit après un palindrome

--- OHCE.Test/OhceTest.cs
+++ OHCE.Test/OhceTest.cs
@@ -16,4 +16,18 @@ public class OhceTest
         // ALORS elle est renvoyée à l'envers
         Assert.Equal(attendu, résultat);
     }
+
+    [Fact]
+    public void PalindromeBienDit()
+    {
+        // ETANT DONNE un palindrome
+        const string palindrome = "kayak";
+
+        // QUAND on l'envoie au détecteur de palindrome
+        var résultat = DétecteurPalindrome.Inverser(palindrome);
+        
+        // ALORS il est renvoyé
+        // ET "Bien dit !" est écrit sur la ligne suivante.
+        Assert.Equal(palindrome + Environment.NewLine + "Bien dit !", résultat);
+    }
 }
